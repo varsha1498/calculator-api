7b5201f962113d329bccedfc0c7c39767646c5cb
Varsha Aggarwal <aggarwalvarsha19@gmail.com>
2020-09-15 20:18:51 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,79 +1,91 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const port = 3000;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
 // your code goes here
 app.get('/', (req,res)=>{
-    return res.status(200).message("Hello world");
+    return res.json("Hello world");
 });
 
 app.post('/add',(req,res)=>{
 
     const {num1,num2} = req.body;
-    if(typeof(num1)||typeof(num2)){
-        return res.status(200).json({
+    if(typeof(num1)==="string"||typeof(num2)==="string"){
+        return res.json({
+            status: "failure",
             message: "Invalid data types"
         })
     }
-    if(num1>1000000||num2>1000000){
-        return res.status(200).json({
-            'message': "overflow"
+   
+    const result = num1 + num2;
+    if(num1>1000000||num2>1000000||result>1000000){
+        return res.json({
+            status: "failure",
+            message : "overflow"
         })
     }
-    const sum = num1 + num2;
-    return res.status(200).json({
-        'message': 'the sum of given two numbers',
-        'sum': sum
+    return res.json({
+        status: 'success',
+        message: 'the sum of given two numbers',
+        sum: result
     })
 
 });
 
 app.post('/sub', (req,res)=>{
     const {num1,num2} = req.body;
-    if(typeof(num1)||typeof(num2)){
-        return res.status(200).json({
+    if(typeof(num1)==="string"||typeof(num2)==="string"){
+        return res.json({
+            status: "failure",
             message: "Invalid data types"
         })
     }
-    if(num1<1000000||num2<1000000){
-        return res.status(200).json({
-            'message': "underflow"
+    
+    const sub = num1 - num2;
+    if(sub<-1000000){
+        return res.json({
+            status: "failure",
+            message: "underflow"
         })
     }
-    const sub = num1 - num2;
-    return res.status(200).json({
-        'message': 'the substract of given two numbers',
-        'sum': sub
+    return res.json({
+        status: "success",
+        message: 'the substract of given two numbers',
+        difference: sub
     })
 
 })
 
 app.post('/multiply', (req,res)=>{
     const {num1,num2} = req.body;
-    if(typeof(num1)||typeof(num2)){
-        return res.status(200).json({
+    if(typeof(num1)==="string"||typeof(num2)==="string"){
+        return res.json({
+            status: "failure",
             message: "Invalid data types"
         })
     }
-    if(num1>1000000||num2>1000000){
-        return res.status(200).json({
-            'message': "overflow"
+   
+
+    const mult = num1*num2;
+    if(mult>1000000){
+        return res.json({
+            status:"failure",
+            message: "overflow"
         })
     }
-
-    const result = num1*num2;
-    return res.status(200).json({
-        'message': 'the product of given numbers',
-        'result': result
+    return res.json({
+        status: "success",
+        message: 'the product of given numbers',
+        result: mult
     })
 
 
@@ -81,16 +93,23 @@ app.post('/multiply', (req,res)=>{
 
 app.post('/divide',(req,res)=>{
     const {num1,num2} = req.body;
-
-    if(num2==0){
-        return res.status(200).json({
-            'message': 'Cannot divide by zero'
+    if(typeof(num1)==="string"||typeof(num2)==="string"){
+        return res.json({
+            status: "failure",
+            message: "Invalid data types"
         });
     }
-    const result = num1/num2;
-    return res.status(200).json({
-        'message': 'the division of given numbers',
-        'result': result
+    if(num2==0){
+        return res.json({
+            status: "failure",
+            message: 'Cannot divide by zero'
+        });
+    }
+    const div = num1/num2;
+    return res.json({
+        status: "success",
+        message: 'the division of given numbers',
+        result: div
     })
 })
 
